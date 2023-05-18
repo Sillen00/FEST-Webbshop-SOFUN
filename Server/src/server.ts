bbab042c46820104594334cd2d9df4fc89b3b90b
Simon <sillen.00@hotmail.com>
2023-05-18 12:57:42 +0200

    Changed db file connection

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -5,7 +5,7 @@ const port = 3000;
 
 async function main() {
   mongoose.set('strictQuery', false); // Detta var bara för att inte få varning i terminalen. Den varnar för en ny version av mongoose där denna används.?
-  await mongoose.connect('mongodb://127.0.0.1:27017/Rhinogram');
+  await mongoose.connect('mongodb://127.0.0.1:27017/WebshopFEST');
   console.log('Connected to Database');
 
   app.listen(port, () => {
