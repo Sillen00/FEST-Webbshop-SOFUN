b39419bcd660f57bd184fd3ceac2751805a5d8f2
Simon <sillen.00@hotmail.com>
2023-05-19 16:01:44 +0200

    Database connected

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,11 +1,19 @@
+import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import { app } from './app';
+dotenv.config();
+// import MONGO_PASSWORD from '/.env';
+
+
+const password = process.env.MONGO_PASSWORD;
 
 const port = 3000;
 
 async function main() {
   mongoose.set('strictQuery', false); // Detta var bara för att inte få varning i terminalen. Den varnar för en ny version av mongoose där denna används.?
-  await mongoose.connect('mongodb://127.0.0.1:27017/WebshopFEST');
+  await mongoose.connect(
+    `mongodb+srv://fest:${password}@fest-webshop.bvzb340.mongodb.net/WebshopApp?retryWrites=true&w=majority`
+  );
   console.log('Connected to Database');
 
   app.listen(port, () => {
