f35d6c077d47d16ae3d93818b1b7bc5b72cde20a
Simon <sillen.00@hotmail.com>
2023-05-22 14:21:30 +0200

    installed argon2 and added a model and endpoint. (WIP)

--- Server/src/resources/users/user-controller.ts
+++ Server/src/resources/users/user-controller.ts
@@ -5,23 +5,43 @@
 // assignAsAdmin()
 
 import { Request, Response } from 'express';
+import { UserModel } from './user-model';
+
+// import { boolean, object, string } from 'zup';
+
+export const UserCreateSchema = object({
+  username: string().required(),
+  password: string().required(),
+  isAdmin: boolean().notRequired(),
+})
+  .noUnknown()
+  .strict();
 
 export async function getAllUsers(req: Request, res: Response) {
-  console.log("Placeholder för getAllUsers");
+  console.log('Placeholder för getAllUsers');
 }
 
 export async function signUpUser(req: Request, res: Response) {
-    console.log("Placeholder för signUpUser");
+  const userBody = await UserCreateSchema.validate(req.body);
+  const existingUser = await UserModel.findOne({ username: userBody.username });
+  if (existingUser) {
+    return res.status(409).json('Username is already taken');
+  }
+
+  const newUser = new UserModel(req.body);
+  const { password, ...userWithoutPass } = newUser.toObject();
+  await newUser.save();
+  res.status(201).json(userWithoutPass);
 }
 
 export async function logInUser(req: Request, res: Response) {
-    console.log("Placeholder för logInUser");
+  console.log('Placeholder för logInUser');
 }
 
 export async function logOutUser(req: Request, res: Response) {
-    console.log("Placeholder för logOutUser");
+  console.log('Placeholder för logOutUser');
 }
 
 export async function assignAsAdmin(req: Request, res: Response) {
-    console.log("Placeholder för assignAsAdmin");
+  console.log('Placeholder för assignAsAdmin');
 }
