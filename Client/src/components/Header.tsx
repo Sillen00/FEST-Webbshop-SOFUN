3fbb47b8de7e82e9284d99a6544ef753dd389b61
Simon <sillen.00@hotmail.com>
2023-05-22 17:01:29 +0200

    Tog bort en onödig box

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -29,107 +29,94 @@ export default function Header() {
         backgroundColor: '#fffaf5',
         width: '100%',
         display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         alignItems: 'center',
         height: '9rem',
         textDecoration: 'none',
         padding: '1rem 5rem 1rem 5rem',
+        color: 'black',
       }}
     >
+      {/* LOGIN MODAL --------------------------------------------------------------------- */}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+        sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+      >
+        <LoginRegisterModal />
+      </Modal>
+
       <Box
         sx={{
-          width: '100%',
-          display: 'flex',
           alignItems: 'center',
-          justifyContent: 'space-between',
-          textAlign: 'center',
+          padding: '0.5rem 0',
+        }}
+      >
+        <NavLink style={{ textDecoration: 'none' }} to='./'>
+          <Logo />
+        </NavLink>
+      </Box>
+      <Box
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          gap: '2rem',
+          padding: '0 1rem',
+          textDecoration: 'none',
+          fontFamily: 'Oswald, sans-serif',
+          fontSize: '1.3rem',
           '& a': {
-            color: 'black',
+            color: 'secondary.contrastText',
             textDecoration: 'none',
+            '&:hover': {
+              color: 'secondary.contrastText',
+            },
           },
         }}
       >
-        {/* LOGIN MODAL --------------------------------------------------------------------- */}
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby='modal-modal-title'
-          aria-describedby='modal-modal-description'
-          sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-        >
-          <LoginRegisterModal />
-        </Modal>
+        <NavLink to='./admin' data-cy='admin-link'>
+          <IconButton aria-label='admin' sx={{ color: 'secondary.contrastText' }}>
+            <Icon.AccountCircle sx={{ fontSize: '1.9rem' }} />
+          </IconButton>
+        </NavLink>
 
-        <Box
-          sx={{
-            alignItems: 'center',
-            padding: '0.5rem 0',
-          }}
-        >
-          <NavLink to='./'>
-            <Logo />
-          </NavLink>
-        </Box>
-        <Box
-          sx={{
-            alignItems: 'center',
-            display: 'flex',
-            gap: '2rem',
-            padding: '0 1rem',
-            textDecoration: 'none',
-            fontFamily: 'Oswald, sans-serif',
-            fontSize: '1.3rem',
-            '& a': {
-              color: 'secondary.contrastText',
-              textDecoration: 'none',
-              '&:hover': {
-                color: 'secondary.contrastText',
-              },
-            },
-          }}
-        >
-          <NavLink to='./admin' data-cy='admin-link'>
-            <IconButton aria-label='admin' sx={{ color: 'secondary.contrastText' }}>
-              <Icon.AccountCircle sx={{ fontSize: '1.9rem' }} />
-            </IconButton>
-          </NavLink>
-
-          <Box>
-            <Tooltip title='Kundvagn'>
-              {isLoggedIn ? (
-                <NavLink to='./checkout'>
-                  <IconButton
-                    aria-label='cart'
-                    data-cy='cart-link'
-                    sx={{ color: 'secondary.contrastText' }}
+        <Box>
+          <Tooltip title='Kundvagn'>
+            {isLoggedIn ? (
+              <NavLink to='./checkout'>
+                <IconButton
+                  aria-label='cart'
+                  data-cy='cart-link'
+                  sx={{ color: 'secondary.contrastText' }}
+                >
+                  <StyledBadge
+                    badgeContent={cart.reduce((total, item) => total + item.quantity, 0) || '0'}
+                    data-cy='cart-items-count-badge'
                   >
-                    <StyledBadge
-                      badgeContent={cart.reduce((total, item) => total + item.quantity, 0) || '0'}
-                      data-cy='cart-items-count-badge'
-                    >
-                      <Icon.ShoppingCart sx={{ fontSize: '1.9rem' }} />
-                    </StyledBadge>
-                  </IconButton>
-                </NavLink>
-              ) : (
-                <NavLink onClick={handleOpen} to={location.pathname}>
-                  <IconButton
-                    aria-label='cart'
-                    data-cy='cart-link'
-                    sx={{ color: 'secondary.contrastText' }}
+                    <Icon.ShoppingCart sx={{ fontSize: '1.9rem' }} />
+                  </StyledBadge>
+                </IconButton>
+              </NavLink>
+            ) : (
+              <NavLink onClick={handleOpen} to={location.pathname}>
+                <IconButton
+                  aria-label='cart'
+                  data-cy='cart-link'
+                  sx={{ color: 'secondary.contrastText' }}
+                >
+                  <StyledBadge
+                    badgeContent={cart.reduce((total, item) => total + item.quantity, 0) || '0'}
+                    data-cy='cart-items-count-badge'
                   >
-                    <StyledBadge
-                      badgeContent={cart.reduce((total, item) => total + item.quantity, 0) || '0'}
-                      data-cy='cart-items-count-badge'
-                    >
-                      <Icon.ShoppingCart sx={{ fontSize: '1.9rem' }} />
-                    </StyledBadge>
-                  </IconButton>
-                </NavLink>
-              )}
-            </Tooltip>
-          </Box>
+                    <Icon.ShoppingCart sx={{ fontSize: '1.9rem' }} />
+                  </StyledBadge>
+                </IconButton>
+              </NavLink>
+            )}
+          </Tooltip>
         </Box>
       </Box>
     </Box>
