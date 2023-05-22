23853e9d302e787bc4b6ca65ad4b4ff25054d2d4
Simon <sillen.00@hotmail.com>
2023-05-22 11:56:16 +0200

    Fått till en modal där man kan byta mellan register och login.

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -1,7 +1,17 @@
 import * as Icon from '@mui/icons-material';
-import { Box, IconButton, Tooltip, useTheme } from '@mui/material';
+import {
+  Box,
+  Button,
+  IconButton,
+  Modal,
+  TextField,
+  Tooltip,
+  Typography,
+  useTheme,
+} from '@mui/material';
 import StyledBadge from '@mui/material/Badge';
 
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useCart } from '../contexts/CartContext';
 import Logo from './Logo';
@@ -9,6 +19,16 @@ import Logo from './Logo';
 export default function Header() {
   const { cart } = useCart();
   const theme = useTheme();
+
+  const isLoggedIn = false; // or false, depending on the user's login status
+
+  const [register, setRegister] = useState(true);
+  // const handleRegisterOpen = () => setRegister(true);
+  // const handleRegisterClose = () => setRegister(false);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <Box
@@ -23,6 +43,92 @@ export default function Header() {
         padding: '1rem',
       }}
     >
+      {register ? (
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby='modal-modal-title'
+          aria-describedby='modal-modal-description'
+          sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+        >
+          <Box sx={{ background: 'white', padding: '4em' }}>
+            <Typography id='modal-modal-title' variant='h6' component='h2'>
+              Logga in
+            </Typography>
+            <Box>
+              <TextField id='standard-error' label='Användarnamn' variant='standard' />
+            </Box>
+            <Box>
+              <TextField id='standard-error' label='Användarnamn' variant='standard' />
+            </Box>
+
+            <Button sx={{ width: '100%', marginTop: '1em' }} variant='contained' color='secondary'>
+              <NavLink style={{ textDecoration: 'none', color: 'white' }} to='./checkout'>
+                Logga in
+              </NavLink>
+            </Button>
+
+            <Box sx={{ border: '1px solid gray' }}></Box>
+
+            <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+              Har du inte ett konto?
+            </Typography>
+            <Button
+              onClick={() => setRegister(false)}
+              sx={{ width: '100%' }}
+              variant='contained'
+              color='secondary'
+            >
+              <NavLink style={{ textDecoration: 'none', color: 'white' }} to=''>
+                Skapa Konto
+              </NavLink>
+            </Button>
+          </Box>
+        </Modal>
+      ) : (
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby='modal-modal-title'
+          aria-describedby='modal-modal-description'
+          sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+        >
+          <Box sx={{ background: 'white', padding: '4em' }}>
+            <Typography id='modal-modal-title' variant='h6' component='h2'>
+              Registrera dig
+            </Typography>
+            <Box>
+              <TextField id='standard-error' label='Användarnamn' variant='standard' />
+            </Box>
+            <Box>
+              <TextField id='standard-error' label='Användarnamn' variant='standard' />
+            </Box>
+
+            <Button sx={{ width: '100%', marginTop: '1em' }} variant='contained' color='secondary'>
+              <NavLink style={{ textDecoration: 'none', color: 'white' }} to='./checkout'>
+                Registrera dig
+              </NavLink>
+            </Button>
+
+            <Box sx={{ border: '1px solid gray' }}></Box>
+
+            <Typography id='modal-modal-description' sx={{ mt: 2 }}>
+              Har redan ett konto?
+            </Typography>
+            <Button
+              onClick={() => setRegister(true)}
+              sx={{ width: '100%' }}
+              variant='contained'
+              color='secondary'
+            >
+              <NavLink style={{ textDecoration: 'none', color: 'white' }} to=''>
+                Logga in
+              </NavLink>
+            </Button>
+          </Box>
+        </Modal>
+      )}
+
       <Box
         sx={{
           alignItems: 'center',
@@ -55,17 +161,31 @@ export default function Header() {
         </NavLink>
         <Box>
           <Tooltip title='Kundvagn'>
-            <NavLink to='./checkout'>
-              <IconButton aria-label='cart' data-cy='cart-link' color='secondary'>
-                <StyledBadge
-                  badgeContent={cart.reduce((total, item) => total + item.quantity, 0) || '0'}
-                  color='info'
-                  data-cy='cart-items-count-badge'
-                >
-                  <Icon.ShoppingCart />
-                </StyledBadge>
-              </IconButton>
-            </NavLink>
+            {isLoggedIn ? (
+              <NavLink to='./checkout'>
+                <IconButton aria-label='cart' data-cy='cart-link' color='secondary'>
+                  <StyledBadge
+                    badgeContent={cart.reduce((total, item) => total + item.quantity, 0) || '0'}
+                    color='info'
+                    data-cy='cart-items-count-badge'
+                  >
+                    <Icon.ShoppingCart />
+                  </StyledBadge>
+                </IconButton>
+              </NavLink>
+            ) : (
+              <NavLink onClick={handleOpen} to=''>
+                <IconButton aria-label='cart' data-cy='cart-link' color='secondary'>
+                  <StyledBadge
+                    badgeContent={cart.reduce((total, item) => total + item.quantity, 0) || '0'}
+                    color='info'
+                    data-cy='cart-items-count-badge'
+                  >
+                    <Icon.ShoppingCart />
+                  </StyledBadge>
+                </IconButton>
+              </NavLink>
+            )}
           </Tooltip>
         </Box>
       </Box>
