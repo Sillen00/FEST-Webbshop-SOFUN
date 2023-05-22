3e666bd2ec02c52e57a56d8105068ecab2f94a3d
Simon <sillen.00@hotmail.com>
2023-05-22 14:11:46 +0200

    Login och register på frontend funkar. next(endpoints)

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -1,28 +1,22 @@
 import * as Icon from '@mui/icons-material';
-import {
-  Box,
-  Button,
-  IconButton,
-  Modal,
-  TextField,
-  Tooltip,
-  Typography,
-  useTheme,
-} from '@mui/material';
+import { Box, IconButton, Modal, Tooltip, useTheme } from '@mui/material';
 import StyledBadge from '@mui/material/Badge';
 
 import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { useCart } from '../contexts/CartContext';
+import LoginRegisterModal from './LoginRegisterModal';
 import Logo from './Logo';
 
 export default function Header() {
   const { cart } = useCart();
   const theme = useTheme();
 
+  const location = useLocation();
+
   const isLoggedIn = false; // or false, depending on the user's login status
 
-  const [register, setRegister] = useState(true);
+  // const [register, setRegister] = useState(true);
   // const handleRegisterOpen = () => setRegister(true);
   // const handleRegisterClose = () => setRegister(false);
 
@@ -43,91 +37,16 @@ export default function Header() {
         padding: '1rem',
       }}
     >
-      {register ? (
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby='modal-modal-title'
-          aria-describedby='modal-modal-description'
-          sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-        >
-          <Box sx={{ background: 'white', padding: '4em' }}>
-            <Typography id='modal-modal-title' variant='h6' component='h2'>
-              Logga in
-            </Typography>
-            <Box>
-              <TextField id='standard-error' label='Användarnamn' variant='standard' />
-            </Box>
-            <Box>
-              <TextField id='standard-error' label='Lösenord' variant='standard' />
-            </Box>
-
-            <Button sx={{ width: '100%', marginTop: '1em' }} variant='contained' color='secondary'>
-              <NavLink style={{ textDecoration: 'none', color: 'white' }} to='./checkout'>
-                Logga in
-              </NavLink>
-            </Button>
-
-            <Box sx={{ border: '1px solid gray', marginTop: "2em" }}></Box>
-
-            <Typography id='modal-modal-description' sx={{ mt: 2 }}>
-              Har du inte ett konto?
-            </Typography>
-            <Button
-              onClick={() => setRegister(false)}
-              sx={{ width: '100%' }}
-              variant='contained'
-              color='secondary'
-            >
-              <NavLink style={{ textDecoration: 'none', color: 'white' }} to=''>
-                Skapa Konto
-              </NavLink>
-            </Button>
-          </Box>
-        </Modal>
-      ) : (
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby='modal-modal-title'
-          aria-describedby='modal-modal-description'
-          sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-        >
-          <Box sx={{ background: 'white', padding: '4em' }}>
-            <Typography id='modal-modal-title' variant='h6' component='h2'>
-              Registrera dig
-            </Typography>
-            <Box>
-              <TextField id='standard-error' label='Användarnamn' variant='standard' />
-            </Box>
-            <Box>
-              <TextField id='standard-error' label='Lösenord' variant='standard' />
-            </Box>
-
-            <Button sx={{ width: '100%', marginTop: '1em' }} variant='contained' color='secondary'>
-              <NavLink style={{ textDecoration: 'none', color: 'white' }} to='./checkout'>
-                Registrera dig
-              </NavLink>
-            </Button>
-
-            <Box sx={{ border: '1px solid gray', marginTop: "2em" }}></Box>
-
-            <Typography id='modal-modal-description' sx={{ mt: 2 }}>
-              Har redan ett konto?
-            </Typography>
-            <Button
-              onClick={() => setRegister(true)}
-              sx={{ width: '100%' }}
-              variant='contained'
-              color='secondary'
-            >
-              <NavLink style={{ textDecoration: 'none', color: 'white' }} to=''>
-                Logga in
-              </NavLink>
-            </Button>
-          </Box>
-        </Modal>
-      )}
+      {/* LOGIN MODAL --------------------------------------------------------------------- */}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+        sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+      >
+        <LoginRegisterModal />
+      </Modal>
 
       <Box
         sx={{
@@ -174,7 +93,7 @@ export default function Header() {
                 </IconButton>
               </NavLink>
             ) : (
-              <NavLink onClick={handleOpen} to=''>
+              <NavLink onClick={handleOpen} to={location.pathname}>
                 <IconButton aria-label='cart' data-cy='cart-link' color='secondary'>
                   <StyledBadge
                     badgeContent={cart.reduce((total, item) => total + item.quantity, 0) || '0'}
