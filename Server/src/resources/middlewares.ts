5ff0ef4e5f5bea907c5a7888ea43bea8b3c995ce
Simon <sillen.00@hotmail.com>
2023-05-24 11:03:25 +0200

    changed authUser to authLogin

--- Server/src/resources/middlewares.ts
+++ Server/src/resources/middlewares.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-export function authUser(req: Request, res: Response, next: NextFunction) {
+export function authLogin(req: Request, res: Response, next: NextFunction) {
     console.log("Placeholder för authUser");
     next();
 }
