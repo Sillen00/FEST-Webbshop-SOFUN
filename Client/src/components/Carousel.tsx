130bdb7066868422343694214fb79c9fca27149e
felicia <feliciawillnas@gmail.com>
2023-05-24 12:18:44 +0200

    updated images

--- Client/src/components/Carousel.tsx
+++ Client/src/components/Carousel.tsx
@@ -8,68 +8,21 @@ export default function ReactBootstrapCarousel() {
         {/* SLIDE 1 */}
         <Carousel.Item>
           <img
-            className='d-block w-100'
-            src='/Images/Frame5.png'
+            className='d-block'
+            src='/Images/Carousel1.png'
             alt='soffa1'
-            style={{ objectFit: 'cover', height: '500px', width: '100%' }}
+            style={{ objectFit: 'cover', width: '100%', maxHeight: '600px' }}
           />
-          <Carousel.Caption>
-            <h3
-              style={{
-                textAlign: 'left',
-                fontSize: '50px',
-              }}
-            >
-              NYHETER
-            </h3>
-            <h3 style={{ textAlign: 'right', fontSize: '40px' }}>Upp till 30%</h3>
-            <p style={{ textAlign: 'right' }}>På flera tre sits soffor</p>
-          </Carousel.Caption>
         </Carousel.Item>
 
         {/* SLIDE 2 */}
         <Carousel.Item>
           <img
-            className='d-block w-100'
-            src='/Images/Frame6.png'
+            className='d-block'
+            src='/Images/Carousel2.png'
             alt='Second slide'
-            style={{ objectFit: 'cover', height: '500px', width: '100%' }}
+            style={{ objectFit: 'cover', width: '100%', maxHeight: '600px' }}
           />
-          <Carousel.Caption style={{ textAlign: 'left' }}>
-            <h3 style={{ textAlign: 'left' }}>TOBAGO</h3>
-            <p style={{ textAlign: 'left' }}>Soffa Cinlay 3-sits i sammet</p>
-            <p style={{ textAlign: 'left' }}>33,000 sek</p>
-            <h3 style={{ textAlign: 'right', color: 'black' }}>CALLO</h3>
-            <p
-              style={{
-                textAlign: 'right',
-                fontSize: '25px',
-                color: '#cc5158',
-              }}
-            >
-              8,000 sek
-            </p>
-            <p style={{ textAlign: 'right', textDecoration: 'line-through', color: 'black' }}>
-              11,000 sek
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-
-        {/* SLIDE 3 */}
-        <Carousel.Item>
-          <img
-            className='d-block w-100'
-            src='/Images/Frame7.png'
-            alt='Third slide'
-            style={{ objectFit: 'cover', height: '500px', width: '100%' }}
-          />
-
-          <Carousel.Caption style={{ textAlign: 'left' }}>
-            <h3 style={{ textAlign: 'left' }}>SHAPE i chenille</h3>
-            <p style={{ textAlign: 'left' }}>30,000 sek</p>
-            <h3 style={{ textAlign: 'right' }}>Skräddarsy din soffa</h3>
-            <p style={{ textAlign: 'right', textDecoration: 'underline' }}>Shoppa nu</p>
-          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </>
