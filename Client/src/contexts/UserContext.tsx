d6ba5cf0b307033d2e7c2fa13d917fd3493fae21
edvindjulic <djulicedvin@gmail.com>
2023-05-26 10:26:26 +0200

    Ran prettier

--- Client/src/contexts/UserContext.tsx
+++ Client/src/contexts/UserContext.tsx
@@ -9,11 +9,11 @@ interface User {
 }
 
 interface UserContextValue {
-  registerUser: (values: {username: string, password: string}) => void;
+  registerUser: (values: { username: string; password: string }) => void;
   open: boolean;
   handleOpen: () => void;
   handleClose: () => void;
-  loginUser: (values: {username: string, password: string}) => void;
+  loginUser: (values: { username: string; password: string }) => void;
   isLoggedIn: boolean;
   isNotValid: boolean;
   fetchAllUsers: () => void;
@@ -54,7 +54,7 @@ export default function UserProvider({ children }: Props) {
       });
   };
 
-  const loginUser = async (values: {username: string, password: string}) => {
+  const loginUser = async (values: { username: string; password: string }) => {
     await axios
       .post(
         '/api/users/signup',
@@ -76,7 +76,7 @@ export default function UserProvider({ children }: Props) {
       });
   };
 
-  const registerUser = async (values: {username: string, password: string}) => {
+  const registerUser = async (values: { username: string; password: string }) => {
     axios
       .post(
         '/api/users/login',
