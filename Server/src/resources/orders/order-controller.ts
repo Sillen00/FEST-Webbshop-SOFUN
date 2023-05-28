d9d53249f1ba39a3e855f9d45fd26954d9af5998
edvindjulic <djulicedvin@gmail.com>
2023-05-28 12:41:20 +0200

    Setup controller and model files. Three schemas in the order model: order, orderitems and adress.

--- Server/src/resources/orders/order-controller.ts
+++ Server/src/resources/orders/order-controller.ts
@@ -4,6 +4,29 @@
 // createOrder()
 
 import { Request, Response } from 'express';
+import * as Yup from 'yup';
+import { OrderModel } from './order-model';
+
+const addressSchema = Yup.object().shape({
+  firstName: Yup.string().required(),
+  lastName: Yup.string().required(),
+  zipCode: Yup.number().required(),
+  city: Yup.string().required(),
+  phoneNumber: Yup.number().required(),
+});
+
+const orderItemSchema = Yup.object().shape({
+  productID: Yup.string().required(),
+  quantity: Yup.number().required(),
+});
+
+const orderSchema = Yup.object().shape({
+  userID: Yup.string().required(),
+  totalPrice: Yup.number().required(),
+  deliveryAddress: addressSchema.required(),
+  isShipped: Yup.boolean(),
+  orderItems: Yup.array().of(orderItemSchema).required(),
+});
 
 export async function getAllOrders(req: Request, res: Response) {
   console.log('Placeholder för getAllOrders');
