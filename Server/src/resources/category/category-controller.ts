47e0cfd1b104b5fba2776b7804a3007c8a92e1ba
felicia <feliciawillnas@gmail.com>
2023-05-29 01:33:55 +0200

    able to get all contents of category

--- Server/src/resources/category/category-controller.ts
+++ Server/src/resources/category/category-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { ProductModel } from "../products/product-model";
 import { CategoryModel } from "./category-model";
 
 
@@ -16,9 +17,44 @@ export async function createCategory(req: Request, res: Response) {
     //      return;
     //    }}
     
-await category.save();
-res.status(201).json(category);
+    await category.save();
+    res.status(201).json(category);
+}
+
+// GET CATEGORY
+export async function getCategory(req: Request, res: Response) {
+    const category = req.params.category;
+    console.log(req.params.category);
+    
+    const products = await ProductModel.find({ categories: category });
+    console.log(products);
+    res.json(products);
+}
    
+// GET ALL CATEGORIES
+export async function getAllCategories(req: Request, res: Response) {
+    const categories = await CategoryModel.find({});
+    res.json(categories);
+}
+
+// GET SPECIFIC CATEGORY
+// export async function getCategoryById(req: Request, res: Response) {
+//     console.log('Placeholder for getCategoryById');
+// }
+
+// GET ALL PRORDUCTS WITH SAME CATEGORY
+export async function getAllContentOfCategory(req: Request, res: Response) {
+    const categoryId = req.params.categoryId; // Assuming the parameter is categoryId
+    
+    try {
+      const products = await ProductModel.find({ categoryIDs: categoryId });
+      res.json(products);
+    } catch (error) {
+      console.error('Error retrieving products:', error);
+      res.status(500).json({ message: 'Error retrieving products' });
     }
+  }
+  
+   
 
 
