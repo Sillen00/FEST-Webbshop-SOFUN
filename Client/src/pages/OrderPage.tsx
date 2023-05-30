0a3a6514b8b8afbd78e67dc8b70a74a68de875a1
edvindjulic <djulicedvin@gmail.com>
2023-05-30 15:28:12 +0200

    Added logout in orderPage

--- Client/src/pages/OrderPage.tsx
+++ Client/src/pages/OrderPage.tsx
@@ -1,13 +1,14 @@
-import { Box, Card, CardContent, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Order, useOrder } from '../contexts/OrderContext';
 import { useUser } from '../contexts/UserContext';
 
 export default function OrderPage() {
   const { getOrdersByUser } = useOrder();
-  const { currentUser } = useUser();
-
+  const { currentUser, logoutUser } = useUser();
   const [userOrders, setUserOrders] = useState<Order[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUserOrders = async () => {
@@ -31,47 +32,62 @@ export default function OrderPage() {
         backgroundColor: 'secondary.main',
       }}
     >
+      <Button
+        variant='contained'
+        color='primary'
+        sx={{ margin: '0 0 5em 0' }}
+        onClick={() => {
+          logoutUser();
+          navigate('/');
+        }}
+      >
+        <Typography variant='body1'>Logga ut </Typography>
+      </Button>
       <Typography variant='h3'>Dina beställningar</Typography>
-      <Box sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        width: '80vw',
-        gap: "1rem",
-      }}>
-      {userOrders.map(order => (
-        <Card
-          key={order._id}
-          variant='outlined'
-          sx={{
-            backgroundColor: 'white',
-            borderBottom: '1px solid black',
-            marginBottom: '1rem',
-            width: '30rem',
-          }}
-        >
-          <CardContent
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          width: '80vw',
+          gap: '1rem',
+        }}
+      >
+        {userOrders.map(order => (
+          <Card
+            key={order._id}
+            variant='outlined'
             sx={{
-              display: 'flex',
-              flexDirection: "column",
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '100%',
+              backgroundColor: 'white',
+              borderBottom: '1px solid black',
+              marginBottom: '1rem',
+              width: '30rem',
             }}
           >
-            <Typography variant='h5'>{order._id}</Typography>
-            <Typography variant='h5'>{order.totalPrice} kr</Typography>
-            <Typography variant='h5'>{order.deliveryAddress.firstName} {order.deliveryAddress.lastName}</Typography>
-            <Typography variant='h5'>{order.deliveryAddress.address}</Typography>
-            <Typography variant='h5'>{order.deliveryAddress.zipCode} {order.deliveryAddress.city}</Typography>
-            <Typography variant='h5'> Skickad{' '}
-                {order.isShipped ? 'Ja' : 'Nej'}
-            </Typography>
-            <Typography variant='h5'>
+            <CardContent
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
+              }}
+            >
+              <Typography variant='h5'>{order._id}</Typography>
+              <Typography variant='h5'>{order.totalPrice} kr</Typography>
+              <Typography variant='h5'>
+                {order.deliveryAddress.firstName} {order.deliveryAddress.lastName}
+              </Typography>
+              <Typography variant='h5'>{order.deliveryAddress.address}</Typography>
+              <Typography variant='h5'>
+                {order.deliveryAddress.zipCode} {order.deliveryAddress.city}
+              </Typography>
+              <Typography variant='h5'> Skickad {order.isShipped ? 'Ja' : 'Nej'}</Typography>
+              <Typography variant='h5'>
                 Skapad: {new Date(order.createdAt).toLocaleDateString('sv-SE')}
               </Typography>
-          </CardContent>
-        </Card>
-      ))}
+            </CardContent>
+          </Card>
+        ))}
       </Box>
     </Box>
   );
