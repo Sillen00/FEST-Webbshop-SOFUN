0f7e4d2c8c0bab4f5471e43c6ff361ebc71b5dfe
edvindjulic <djulicedvin@gmail.com>
2023-06-01 00:53:29 +0200

    Styling. WIP

--- Client/src/pages/OrderPage.tsx
+++ Client/src/pages/OrderPage.tsx
@@ -72,12 +72,32 @@ export default function OrderPage() {
               },
             }}
           >
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                gap: '1rem',
+                width: '100%',
+                padding: '0 0.4rem',
+                borderBottom: '1px solid lightgrey',
+              }}
+            >
+              <Typography variant='h5'>
+                Ordern skapades:{' '}
+                {new Date(order.createdAt).toLocaleDateString('sv-SE', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric',
+                })}
+              </Typography>
+            </Box>
             <CardContent
               sx={{
                 display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                height: '100%',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                gap: '0.5rem',
+                height: 'auto',
                 padding: 0,
                 '&:last-child': {
                   paddingBottom: 0,
@@ -87,35 +107,16 @@ export default function OrderPage() {
               <Box
                 sx={{
                   display: 'flex',
-                  flexDirection: 'row',
-                  gap: '1rem',
-                  width: '100%',
-                  padding: '0 0.4rem',
-                  borderBottom: '1px solid lightgrey',
-                }}
-              >
-                <Typography variant='h5'>
-                  Ordern skapades:{' '}
-                  {new Date(order.createdAt).toLocaleDateString('sv-SE', {
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric',
-                  })}
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  display: 'flex',
                   flexDirection: 'column',
                   gap: '0.4rem',
-                  alignItems: 'center',
                   justifyContent: 'center',
                   flexWrap: 'wrap',
-                  width: '10rem',
+                  maxWidth: '10rem',
                   padding: '0.4rem',
+                  marginBottom: '0.5rem',
                 }}
               >
-                {order.orderItems.map((item, index) => (
+                {order.orderItems.map((orderItem, index) => (
                   <Box
                     key={index}
                     sx={{
@@ -126,16 +127,33 @@ export default function OrderPage() {
                     }}
                   >
                     <img
-                      src={`/api/image/${item.productID.imageID}`}
-                      alt={item.productID.title}
-                      style={{ width: '10rem', height: 'auto' }}
+                      src={`/api/image/${orderItem.productID.imageID}`}
+                      alt={orderItem.productID.title}
+                      style={{ maxWidth: '100%', height: 'auto' }}
                     />
-                    <Typography variant='h5'>{item.productID.title}</Typography>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                      }}
+                    >
+                      <Typography variant='h5'>{orderItem.productID.title}</Typography>
+                      <Typography variant='h5'>{orderItem.quantity}x</Typography>
+                    </Box>
                   </Box>
                 ))}
               </Box>
-              <Box>
-              <Typography variant='h5'>Levereras till</Typography>
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  padding: '0.4rem',
+                  marginBottom: '0.5rem',
+                  maxWidth: '25%',
+                  justifyContent: 'flex-end',
+                }}
+              >
+                <Typography variant='h5'>Levereras till</Typography>
                 <Typography variant='h5'>
                   {order.deliveryAddress.firstName} {order.deliveryAddress.lastName}
                 </Typography>
@@ -143,11 +161,10 @@ export default function OrderPage() {
                 <Typography variant='h5'>
                   {order.deliveryAddress.zipCode} {order.deliveryAddress.city}
                 </Typography>
-                <Typography variant='h5'>
-                  {order.deliveryAddress.zipCode} {order.deliveryAddress.city}
-                </Typography>
                 <Typography variant='h5'>{order.deliveryAddress.phoneNumber}</Typography>
-                <Typography variant='h5'>{order.isShipped ? 'Ordern är skickad' : 'Ordern behandlas'}</Typography>
+                <Typography variant='h5'>
+                  {order.isShipped ? 'Ordern är skickad' : 'Ordern behandlas'}
+                </Typography>
               </Box>
             </CardContent>
           </Card>
@@ -156,4 +173,3 @@ export default function OrderPage() {
     </Box>
   );
 }
-
